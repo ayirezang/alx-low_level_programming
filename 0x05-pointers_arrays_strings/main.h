89a5968e3ce0ee5b2fc9930d3d8a64e9d76e6337
ayirezang <patienceayirezang33@gmail.com>
2023-06-26 12:48:25 -0700

    main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 
+int _putchar(char c):
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,7 +11,7 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-void print_array(int *a, int n);
+void print_array(int *a, int n)i;
 char *_strcpy(char *dest, char *src);
 
 #endif
